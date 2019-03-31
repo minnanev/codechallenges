04b7d2a464984fff0d15aa64653d9cbb53fbe5ba
Minna Nevalainen <minna@Minnas-MacBook-Air.local>
2019-03-31 21:20:50 +0300

    changes to pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,13 @@
 def pipeline(*funcs):
     def helper(arg):
         # YOUR CODE GOES HERE
-        use (*funcs)
-        for each (funcs in func):
-            arg = func(arg)
-            return pipeline()
-        return helper
+        for (arg) in funcs:
+            return pipeline(arg)
+
+    return helper
+
+# I did not  get this to work, I missed the class where pipelines were discussed.
+# I'll continue working on this while the results are being examined, sorry about that!
 
 fun = pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2)
 print(fun(3))
